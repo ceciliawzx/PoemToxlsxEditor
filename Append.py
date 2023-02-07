3d1ba3ab64a088d4229c9a6daa558f5f5a9fd892
Cecilia Wang <ceciliawang@dyn3147-126.wlan.ic.ac.uk>
2023-02-07 09:40:04 +0000

    add AskInput

--- Append.py
+++ Append.py
@@ -5,13 +5,25 @@ This is a program to append content to an existed excel file with specific sheet
 It will automatically calculate the start row by looking at the max_row of cur sheet.
 """
 
-# The example file name is test.xlsx here, should be replaced by the real file.
-writer = pd.ExcelWriter('test.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
-# '诗词' is an example sheet name.
-start_row = writer.sheets['诗词'].max_row
+def AskInput():
+    stop = False
+    append_list = []
+    while not stop:
+        new_poem: str = input("Please input new poem and author, divided by space.\n")
+        alist = new_poem.split()
+        append_list.append(alist)
+        stop = input("Add more?\n0 for yes, 1 for no\n") == "1"
+    return append_list
 
-# df is an example DataFrame.
-df = pd.DataFrame(['a', 'b', 'c'])
-df.index += start_row
-df.to_excel(writer, sheet_name='诗词', startrow=start_row, header=False)
-writer.close()
+def Append():
+    writer = pd.ExcelWriter('result.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
+    # '诗词' is an example sheet name.
+    start_row = writer.sheets['诗词'].max_row
+    the_list = AskInput()
+    # df is an example DataFrame.
+    df = pd.DataFrame(the_list)
+    df.index += start_row
+    df.to_excel(writer, sheet_name='诗词', startrow=start_row, header=False)
+    writer.close()
+
+Append()
